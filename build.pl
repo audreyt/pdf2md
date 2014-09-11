ddc7ff662f8afceade92100d63d0ab75b83cc3f4
Audrey Tang <audreyt@audreyt.org>
2014-09-12 04:49:41 +0800

    * First cut at SVG matching

---
+++ build.pl
@@ -0,0 +1,32 @@
+use strict;
+use Storable qw(nstore);
+local @ARGV = 'kaiu.svg';
+local $/ = "/>";
+my (%PathToCode, %CodeToPath);
+while (<>) {
+   s/\s+/ /g;
+   next unless /<glyph[^>]*unicode="([^"]+)"[^>]*d="([^"]+)/;
+   $PathToCode{$2} = $1;
+   $CodeToPath{$1} = $2;
+}
+local @ARGV = 'f1.svg';
+use HTML::Entities qw[ decode_entities ];
+my %Map;
+while (<>) {
+   s/\s+/ /g;
+   next unless /<glyph[^>]*unicode="([^"]+)"[^>]*d="([^"]+)/;
+   $Map{decode_entities($1)} = decode_entities($PathToCode{$2});
+}
+
+local $/;
+local @ARGV = 'pta.json';
+my $html = <>;
+use Encode;
+Encode::_utf8_on($html);
+$html =~ s<"T":"\K([^"]*)><
+    my $x = $1;
+    $x =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+    $x = Encode::decode_utf8($x);
+    join '', map { $Map{$_} // $_ } split(//, $x);
+>eg;
+print $html;
